17dc2c791f8d5959d558e561555e1db6274cb3f8
pedrocastr <102086416+pedrocastr2@users.noreply.github.com>
2024-10-10 18:55:09 -0300

    Add files via upload

---
+++ escreverloop/src/escreverloop/Main.java
@@ -0,0 +1,17 @@
+package escreverloop;
+
+public class Main {
+ public static void main(String[] args) {
+	
+	String texto ="Um exemplo de texto";
+	int repet = 4;
+	
+	
+	for(int i = 0 ; i<repet; i++) {
+		 System.out.println(texto);
+	}
+	
+	
+}
+}
+
